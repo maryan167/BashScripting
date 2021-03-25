5f29a1e66eb3d26f7e290440e0ad6d0f221fafb5
maryan167 <maryan9876543210@gmail.com>
2021-03-25 15:34:48 +0200

    Update python_v

--- python_v.sh
+++ python_v.sh
@@ -2,8 +2,8 @@
 
 python2_version=$(python2 --version 2>&1 | cut -f2 -d' ')
 python3_version=$(python3 --version 2>/dev/null | cut -f2 -d' ')
-echo $python2_version
-echo $python3_version
+#echo $python2_version
+#echo $python3_version
 
 if [ ! -z "$python2_version" ] || [ ! -z "$python3_version" ]
 then
